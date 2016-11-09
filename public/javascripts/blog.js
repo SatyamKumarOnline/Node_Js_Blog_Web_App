7475feca788942b4d8123554e9fd02e276c7b25c
satyam.kumar <satyam.kumar@synacor.com>
2016-11-09 08:23:37 +0530

    Added code for showing response in UI and show date of blog creation also.

--- public/javascripts/blog.js
+++ public/javascripts/blog.js
@@ -27,13 +27,30 @@ Blog.showAllBlog = function(ev){
         async : true
     }).done(function(resData){
         var blogData = resData;
+        Blog.constructBlogUI(resData);
         console.log(resData);
         // write code to show the JSON response data in UI.
     });
-
 };
 
-Blog.constructBlogUI = function(){
+Blog.constructBlogUI = function(resData){
+    blogDataNode = document.querySelector('[data-blog="detail"]');
+    var _div = document.querySelector('.container');
+    resData.forEach(function(value,key){
+        if(value) {
+            console.log("value is :: ",value);
+            var title = value.title,
+                content = value.content,
+                blogDate = value.DOC,
+                clonedBlogDataNode = blogDataNode.cloneNode(true);
+
+            clonedBlogDataNode.getElementsByTagName('h4')[0].innerText = title;
+            clonedBlogDataNode.getElementsByTagName('p')[0].innerText = content;
+
+            _div.appendChild(clonedBlogDataNode);
+
+        }
+    });
 };
 
 
